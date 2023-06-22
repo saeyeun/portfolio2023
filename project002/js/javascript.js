712af2e312454e882aa022022bd9099ce31c2c93
saeyeun <129368951+saeyeun@users.noreply.github.com>
2023-06-22 16:50:12 +0900

    0622

--- project002/js/javascript.js
+++ project002/js/javascript.js
@@ -46,15 +46,6 @@ $(document).ready(function () {
             $('.story').removeClass('on')
         }
 
-        //history 영역 - 오른쪽 이미지와 텍스트영역이 우측에서 날아옴
-        let bb = $('#wrap .history').offset().top;
-        if (sc >= bb-200) {
-            $('.history .right').addClass('on')
-        }
-        if (sc < bb-200) {
-            $('.history .right').removeClass('on')
-        }
-
         // vision 영역 - 2개의 원이 순차적으로 아래에서 위로 나타나게 설정
         let cc = $('#wrap .vision').offset().top;
         if (sc >= cc-200) {
@@ -106,11 +97,7 @@ $(document).ready(function () {
 
     // 서브페이지
     // 수량부분에서 마이너스와 플러스를 클릭했을 때, 숫자가 늘어나거나 줄어들게 설정
-    // 장바구니버튼을 눌렀을 때, 상단 오른쪽 카트 아이콘의 숫자가 변경
     let i = 1;
-    $('.product .p_wrap .btn_wrap .cartBtn').click(function(){
-        $('#h_wrap header .nav_wrap .h_r_btn ul>li>p').text(i+1)
-    })
     
     $('.product .p_wrap .p_buy .num ul li').eq(2).click(function(){
         $('.product .p_wrap .p_buy .num ul li').eq(1).text(i++)
@@ -118,10 +105,6 @@ $(document).ready(function () {
 
     $('.product .p_wrap .p_buy .num ul li').eq(0).click(function(){
         $('.product .p_wrap .p_buy .num ul li').eq(1).text(i--)
-    })
-
-    $('.product .p_wrap .btn_wrap .cartBtn').click(function(){
-        $('#h_wrap header .nav_wrap .h_r_btn ul>li>p').text(i-1)
     })
 
 });//자바스크립트 끝
